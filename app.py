cc3ce61e3de8993a9d74fbb2152e99f80779ad76
Kevin7744 <arapbiisubmissions@gmail.com>
2024-01-27 12:41:34 +0300

    Update

--- app.py
+++ app.py
@@ -140,3 +140,33 @@ chart_agent = create_agent(
     system_message="Any charts you display will be visible by the user"
 )
 chart_node = functools.partial(agent_node, agent=chart_agent, name="Chart Generator")
+
+
+# Define Tool Node
+tools = [tavily_tool, python_repl]
+tool_executor = ToolExecutor(tools)
+
+def tool_node(state):
+    """
+    This runs tools in the graph
+    It takes in an agent action and calls that tool and returns the result. 
+    """
+    messages = state["messages"]
+    last_message = messages[-1]
+    tool_input = json.loads(
+        last_message.additional_kwargs["function_call"]["arguments"]
+    )
+    if len(tool_input) == 1 and "__arg1" in tool_input:
+        tool_input = next(iter(tool_input.values()))
+    tool_name = last_message.additional_kwargs["function_call"]["name"]
+    action = ToolInvocation(
+        tool = tool_name,
+        tool_input = tool_input,
+    )
+
+    response = tool_executor.invoke(action)
+    funtion_message = FunctionMessage(
+        content=f"{tool_name} response: {str(response)}", name=action.tool
+    )
+
+    return {"messages": [funtion_message]}
